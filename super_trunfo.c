06385976d10c25cb10b17f383c75a8fa4864628f
atrevosa <ju.leonel13@gmail.com>
2025-03-23 16:22:43 +0000

    alterações 2

--- super_trunfo.c
+++ super_trunfo.c
@@ -15,8 +15,30 @@ codigocarta - letra do estado + 2 numeros de 01 - 04;   cidade - nome da cidade
 hab - população da cidade ; ptur - quantidade de pontos turisticos ; area : area em km² ; pib - pib da cidade. */
 
 printf("Digite a primeira letra do Estado: \n");
-scanf()
+scanf("%s"); (&estado);
+
+printf("Digite um numero de 01-04: \n");
+scanf("%s"); (&codigocarta);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s"); (&cidade);
+
+printf("Digite a quantidade de habitantes da cidade", &cidade, ": \n");
+scanf("A quantidade de habitantes da cidade", &cidade, "é de: %d"); (&hab);
+
+printf("Digite a quantidade de pontos turisticos da cidade: \n");
+scanf("A quantidade de pontos turisticos da cidade", &cidade, "é de: %d"); (&ptur);
 
 
+
+
+
+//impressao
+
+printf("Estado: %s -  Codigo da Carta: %s, %s \n", estado, estado, codigocarta);
+printf("Nome da Cidade: %s \n", cidade);
+prinft("Quantidade de Habitantes: %d - Quantidade de Pontos Turísticos: %d", hab, ptur);
+
+return 0;
 
 }
