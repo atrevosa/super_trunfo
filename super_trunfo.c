6f98e56999dbd7f8946a47b82b679930b7e7e647
atrevosa <ju.leonel13@gmail.com>
2025-03-23 16:02:46 +0000

    alterações 1

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,8 +2,21 @@
 
 int main(){
 
+    char estado[1];
+    char codigocarta[2];
+    char cidade[20];    //caracteres
+    int hab;
+    int ptur;
+    float area;
+    float pib;
+   
+/* estado - uma letra de A - H (utiliza-se apenas uma letra para poder funcionar o input do usuario)
+codigocarta - letra do estado + 2 numeros de 01 - 04;   cidade - nome da cidade 
+hab - população da cidade ; ptur - quantidade de pontos turisticos ; area : area em km² ; pib - pib da cidade. */
+
+printf("Digite a primeira letra do Estado: \n");
+scanf()
 
 
 
-    
 }
